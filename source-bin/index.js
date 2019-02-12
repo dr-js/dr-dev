b1bbb44c2e7df2f335ee18225ddd7f754838a14b
dr-js <dr@dr.run>
2019-02-13 00:20:31 +0800

    dr-dev@0.0.3-dev.1
    notable change:
    - use `dr-js@0.20.0-dev.1`

--- source-bin/index.js
+++ source-bin/index.js
@@ -7,42 +7,52 @@ import { doStepPackageVersion } from './stepPackageVersion'
 import { parseOption, formatUsage } from './option'
 import { name as packageName, version as packageVersion } from '../package.json'
 
-const runMode = async ({ isCheckOutdated, isPack, isStepPackageVersion }, { getOptionOptional, getSingleOption, getSingleOptionOptional }) => {
+const runMode = async (
+  { tryGet, getFirst, tryGetFirst },
+  isCheckOutdated,
+  isPack,
+  isStepPackageVersion
+) => {
   isCheckOutdated && await doCheckOutdated({
-    pathInput: getSingleOption('path-input'),
-    pathTemp: getSingleOptionOptional('path-temp')
+    pathInput: getFirst('path-input'),
+    pathTemp: tryGetFirst('path-temp')
   })
   isPack && await doPack({
-    pathInput: getSingleOption('path-input'),
-    pathOutput: getSingleOption('path-output'),
-    outputName: getSingleOptionOptional('output-name'),
-    outputVersion: getSingleOptionOptional('output-version'),
-    outputDescription: getSingleOptionOptional('output-description'),
-    isPublish: getOptionOptional('publish'),
-    isPublishDev: getOptionOptional('publish-dev')
+    pathInput: getFirst('path-input'),
+    pathOutput: getFirst('path-output'),
+    outputName: tryGetFirst('output-name'),
+    outputVersion: tryGetFirst('output-version'),
+    outputDescription: tryGetFirst('output-description'),
+    isPublish: tryGet('publish'),
+    isPublishDev: tryGet('publish-dev')
   })
   isStepPackageVersion && await doStepPackageVersion({
-    pathInput: getSingleOptionOptional('path-input') || '.',
-    isSortKey: getOptionOptional('sort-key'),
-    isGitCommit: getOptionOptional('git-commit')
+    pathInput: tryGetFirst('path-input') || '.',
+    isSortKey: tryGet('sort-key'),
+    isGitCommit: tryGet('git-commit')
   })
 }
 
 const main = async () => {
   const optionData = await parseOption()
-  const { getOptionOptional } = optionData
+  const { tryGet } = optionData
 
-  const isCheckOutdated = getOptionOptional('check-outdated')
-  const isPack = getOptionOptional('pack')
-  const isStepPackageVersion = getOptionOptional('step-package-version')
+  const isCheckOutdated = tryGet('check-outdated')
+  const isPack = tryGet('pack')
+  const isStepPackageVersion = tryGet('step-package-version')
 
   if (!isCheckOutdated && !isPack && !isStepPackageVersion) {
-    return getOptionOptional('version')
+    return tryGet('version')
       ? console.log(JSON.stringify({ packageName, packageVersion }, null, '  '))
-      : console.log(formatUsage(null, getOptionOptional('help') ? null : 'simple'))
+      : console.log(formatUsage(null, tryGet('help') ? null : 'simple'))
   }
 
-  await runMode({ isCheckOutdated, isPack, isStepPackageVersion }, optionData).catch((error) => {
+  await runMode(
+    optionData,
+    isCheckOutdated,
+    isPack,
+    isStepPackageVersion
+  ).catch((error) => {
     console.warn(`[Error]`, error.stack || error)
     process.exit(2)
   })
