bf5de4b369846b6a4112ba061097ba50ede77fe3
dr-js <dr@dr.run>
2021-06-01 19:31:42 +0800

    CHG: use local babel config

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,12 @@
-const BABEL_ENV = process.env.BABEL_ENV || ''
-const isDev = BABEL_ENV.includes('dev')
-const isModule = BABEL_ENV.includes('module')
-const isOutputBin = BABEL_ENV.includes('outputBin') // map `source/*` to `../library/*` for `source-bin` in output
+const { resolve } = require('path')
+const { readFileSync } = require('fs')
+const { runSync } = require('@dr-js/core/library/node/run.js')
 
-module.exports = {
-  presets: [
-    [ '@babel/preset-env', { targets: { node: '12' }, modules: isModule ? false : 'commonjs' } ]
-  ],
-  plugins: [
-    [ 'babel-plugin-minify-replace', {
-      replacements: [ { identifierName: '__DEV__', replacement: { type: 'booleanLiteral', value: isDev } } ]
-    } ],
-    [ 'babel-plugin-module-resolver', {
-      root: [ './' ],
-      alias: isModule ? undefined : [
-        isOutputBin && { '^source/(.+)': './library/\\1' }, // when build bin to output
-        { '^@dr-js/([\\w-]+)/module/(.+)': '@dr-js/\\1/library/\\2' }
-      ].filter(Boolean)
-    } ]
-  ],
-  comments: false
-}
+module.exports = JSON.parse(String( // NOTE: hack to load ES module in sync mode
+  runSync([
+    process.execPath, // node
+    '--input-type=module', // https://nodejs.org/api/packages.html#packages_determining_module_system
+    '--eval',
+    `${String(readFileSync(resolve(__dirname, './source/babel.js')))}; console.log(JSON.stringify(getBabelConfig()))`
+  ], { quiet: true, describeError: true }).stdout
+))
