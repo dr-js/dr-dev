76c77a270ba683f23324a2201c3968fe1a39bc2a
dr-js <dr@dr.run>
2021-11-04 11:41:39 +0800

    DEPRECATE: move `eslint-config-almost-standard-*` to main package

--- resource/eslint/[react]/INIT#.eslintrc.js
+++ resource/eslint/[react]/INIT#.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
-    './node_modules/@dr-js/dev-{FLAVOR}/EXPORT/eslint-config-almost-standard-v14',
-    './node_modules/@dr-js/dev-{FLAVOR}/EXPORT/eslint-config-almost-standard-jsx-v8'
+    './node_modules/@dr-js/dev/resource/eslint-config-almost-standard-v14',
+    './node_modules/@dr-js/dev/resource/eslint-config-almost-standard-jsx-v8'
   ]
 }
